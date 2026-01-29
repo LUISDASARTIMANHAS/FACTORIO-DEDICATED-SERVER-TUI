6434a78078655d4bd2f77a3e81b96e9e45eba31c
HELLOKITTY-I5-L <luisaugustodesouza785@gmail.com>
2026-01-28 22:24:25 -0300

    updated

--- config.cmd
+++ config.cmd
@@ -1,12 +1,20 @@
 @echo off
 
+
 :: Diretórios
+:: save padrão a usar // default save
+set SAVE_NAME=mephisto-serie-11
+
+:: default localtion C:\Users\user\AppData\Roaming\Factorio\saves
+set SAVE_DIR=Z:\SteamLibrary\steamapps\common\Factorio\saves
+set SETTINGS_NAME=server-settings.json
+
+:: GLOBAL VARS (Do not touch, edit at your own risk.) // não toque , edite por sua conta e risco
 set FACTORIO_DIR=%~dp0
 set FACTORIO_EXE=%FACTORIO_DIR%bin\x64\factorio.exe
-set SAVE_DIR=Z:\SteamLibrary\steamapps\common\Factorio\saves
-set SAVE_NAME=mephisto-serie-11
-set SETTINGS_FILE=%FACTORIO_DIR%data\server-settings.json
+set FACTORIO_DATA_DIR=%FACTORIO_DIR%data
 set LOG_DIR=%FACTORIO_DIR%logs
+set SETTINGS_FILE=%FACTORIO_DATA_DIR%%SETTINGS_NAME%
 
 :: Execução
 set PRIORITY=high
